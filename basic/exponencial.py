5598129cabf11eb5821902a036a4f4e69b28afb9
lipewinning <felipealvesdesousa@gmail.com>
2019-01-26 10:23:18 +0000

    List and exponencial function

---
+++ basic/exponencial.py
@@ -0,0 +1,13 @@
+
+
+def raise_to_power(base_num, power_num):
+    result = 1
+    for index in range(power_num):
+        result = result * base_num
+
+    return result
+
+print(raise_to_power(2, 3))
+print(raise_to_power(10,0))
+
+
